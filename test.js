894ac2b453bf12df7660e1f8c35cbecb7731b374
turtlesteak <81275996+turtlesteak@users.noreply.github.com>
2022-03-23 12:59:08 -0400

    Update test.js
    Updated js code allows for run on click of the HTML button

--- test.js
+++ test.js
@@ -1,14 +1,11 @@
 var Powers = ["Homing", "Jump", "Brake", "Thick", "One Shot", "Mine", "Zap", "Low-res", "Thin", "Scatter Shot", "Steer-less", "Speed Burst", "Stealth Mine", "Trippy", "Power Dash", "Side Shot", "Speedy", "Shield", "Clock Block", "Time Bomb", "Hide Self", "Curve-blind", "Angle Turns", "Multi Shot", "Trigger Bomb", "180", "Double Shot", "Reverse", "Laser", "Teleport"];
-
 function roll() {
     var a = Math.floor(Math.random() * Powers.length);
     var b = Math.floor(Math.random() * Powers.length);
     if (a == b) {
-        // document.getElementById("roll").innerHTML = 
-        console.log(Powers[a] + "&" + Powers[b] + ": Duplicate found, rerolling...");
+        console.log(Powers[a] + " & " + Powers[b] + ": Duplicate found, rerolling...");
         roll();
     } else {
-        document.getElementById("roll").innerHTML = ("Power 1:<br/>" + Powers[a] + "<br/>Power 2:<br/>" +          Powers[b] + "<br/><br/>Good luck and have fun :D<br/>");
+        document.getElementById("roll").innerHTML = ("Power 1:<br/>" + Powers[a] + "<br/>Power 2:<br/>" + Powers[b] + "<br/><br/>Good luck and have fun :D<br/>");
     }
 }
-roll()
