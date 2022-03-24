ad628b4288e218ca412d8ff65c0006c95fdc4ac1
turtlesteak <81275996+turtlesteak@users.noreply.github.com>
2022-03-24 01:47:38 -0400

    UnlockedPowers addition
    Addition of user-limited powers via html select tag. This limits the powers available to roll.

--- test.js
+++ test.js
@@ -1,11 +1,14 @@
-var Powers = ["Homing", "Jump", "Brake", "Thick", "One Shot", "Mine", "Zap", "Low-res", "Thin", "Scatter Shot", "Steer-less", "Speed Burst", "Stealth Mine", "Trippy", "Power Dash", "Side Shot", "Speedy", "Shield", "Clock Block", "Time Bomb", "Hide Self", "Curve-blind", "Angle Turns", "Multi Shot", "Trigger Bomb", "180", "Double Shot", "Reverse", "Laser", "Teleport"];
+let Powers = ["Homing", "Jump", "Brake", "Thick", "One Shot", "Mine", "Zap", "Low-res", "Thin", "Scatter Shot", "Steer-less", "Speed Burst", "Stealth Mine", "Trippy", "Power Dash", "Side Shot", "Speedy", "Shield", "Clock Block", "Time Bomb", "Hide Self", "Curve-blind", "Angle Turns", "Multi Shot", "Trigger Bomb", "180", "Double Shot", "Reverse", "Laser", "Teleport"];
 function roll() {
-    var a = Math.floor(Math.random() * Powers.length);
-    var b = Math.floor(Math.random() * Powers.length);
+    let select = document.getElementById("powersList");
+    let UnlockedPowers = select.value;
+    let a = Math.floor(Math.random() * UnlockedPowers);
+    let b = Math.floor(Math.random() * UnlockedPowers);
     if (a == b) {
         console.log(Powers[a] + " & " + Powers[b] + ": Duplicate found, rerolling...");
         roll();
     } else {
-        document.getElementById("roll").innerHTML = ("Power 1:<br/>" + Powers[a] + "<br/>Power 2:<br/>" + Powers[b] + "<br/><br/>Good luck and have fun :D<br/>");
+        document.getElementById("Power 1").innerHTML = (Powers[a]);
+        document.getElementById("Power 2").innerHTML = (Powers[b] + "<br/><br/>Good luck and have fun :D");
     }
 }
